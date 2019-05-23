47cece0e46e43c6b37db5e8dc997be420dc05852
Keos99 <keos99@bk.ru>
2019-05-23 09:51:01 +0300

    Add button and button click listener (Kotlin)

--- app/src/main/java/com/example/gu_kotlin_for_android/MainActivity.kt
+++ app/src/main/java/com/example/gu_kotlin_for_android/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.gu_kotlin_for_android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val button: Button = findViewById(R.id.MainButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
+    }
+
+    fun initUI (){
+        button.setOnClickListener {
+            doBOOM()
+        }
+    }
+
+    fun doBOOM (){
+        Toast.makeText(this,"BOOOOOOM!!!",Toast.LENGTH_LONG)
     }
 }
